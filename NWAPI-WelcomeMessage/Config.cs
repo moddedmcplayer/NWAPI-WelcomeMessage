0290350a1407c9dbb1f8da0e18a77ce04d942927
moddedmcplayer <moddedmcplayer@gmail.com>
2022-09-17 14:47:36 +0200

    xd

--- NWAPI-WelcomeMessage/Config.cs
+++ NWAPI-WelcomeMessage/Config.cs
@@ -1,7 +1,12 @@
 ﻿namespace NWAPI_WelcomeMessage
 {
+    using System.ComponentModel;
+
     public class Config
     {
-        
+        [Description("The message to display to players when they join the server.")]
+        public string WelcomeMessage { get; set; } = "Welcome %playername%!";
+        [Description("The duration of the message in seconds.")]
+        public ushort Duration { get; set; } = 5;
     }
 }
--- NWAPI-WelcomeMessage/Plugin.cs
+++ NWAPI-WelcomeMessage/Plugin.cs
@@ -1,6 +1,27 @@
 ﻿namespace NWAPI_WelcomeMessage
 {
-    public class Class1
+    using MEC;
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+
+    public class Plugin
     {
+        [PluginEntryPoint("WelcomeMessage", "1.0.0", "Displays a welcome message when users join.", "moddedmcplayer")]
+        void Enabled()
+        {
+        }
+
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        void OnPlayerJoin(Player player)
+        {
+            Timing.CallDelayed(1f, () =>
+            {
+                player.SendBroadcast(Config.WelcomeMessage.Replace("%playername", player.Nickname), Config.Duration);
+            });
+        }
+
+        [PluginConfig]
+        public Config Config;
     }
 }
